2b8cd5ac4c13d3b13bfc856fa1d98ed4888687c6
quyvo96 <quyvo9796@gmail.com>
2022-12-17 20:41:04 +0700

    s13 start

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-import React, { useCallback, useMemo, useState } from 'react';
-import Button from './components/UI/Button/Button'
-import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
-import DemoList from './components/Demo/DemoList';
+import Users from './components/Users';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
-  console.log('App test');
-  const toggleparagrapHandler = useCallback(() => {
-    if(allowToggle){
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }
-  }, [allowToggle]);
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  };
-  const [listTitle, setListTitle] = useState('My List');
-
-  const changeTitleHandler = useCallback(() => {
-    setListTitle('New Title');
-  }, []);
-
-  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
-      <DemoList title={listTitle} items={listItems} />
-      <Button onClick={changeTitleHandler}>Change List Title</Button>
-      <DemoOutput show={showParagraph}/>
-      <Button onClick={allowToggleHandler}>allow toggle</Button>
-      <Button onClick={toggleparagrapHandler}>toggle paragrap</Button>
+    <div>
+      <Users />
     </div>
   );
 }
