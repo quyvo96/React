d0ba01e3262e865204449e7eb2706d6246626cb2
quyvo96 <quyvo9796@gmail.com>
2023-01-16 22:06:07 +0700

    add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "30d488b58cb5f70a"; }
+/******/ 		__webpack_require__.h = function() { return "54b303d5e136d159"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
